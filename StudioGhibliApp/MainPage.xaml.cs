9a40fef33b0fafd34c8d6175e5d41e86ae5523c4
s-huel <s-huel@51S500152270>
2024-09-18 11:53:49 -0700

    Add project files.

---
+++ StudioGhibliApp/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace StudioGhibliApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btnHome_Click(object sender, RoutedEventArgs e)
+        {
+            ContentDialog dialog = new ContentDialog
+            {
+                Title = "Home Button",
+                Content = "Going to Home!",
+                CloseButtonText = "OK"
+            };
+
+            /*dialog.ShowAsync();*/
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            // Navigate back to the previous page
+            if (Frame.CanGoBack)
+            {
+                Frame.GoBack();
+            }
+            else
+            {
+                // Optionally show a message if there are no pages to go back to
+                ContentDialog dialog = new ContentDialog
+                {
+                    Title = "Back Button",
+                    Content = "No pages to go back to.",
+                    CloseButtonText = "OK"
+                };
+
+                dialog.ShowAsync();
+            }
+        }
+
+        private void btnFore_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
